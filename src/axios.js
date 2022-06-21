17841239a5a872285ad254c96a78a7f210894ad6
DaveArtDev <42351237+daveartdev@users.noreply.github.com>
2022-06-21 09:39:44 -0600

    Delete axios.js

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"; 
-
-const instance = axios.create({
-    baseURL: "https://us-central1-clone-2e61d.cloudfunctions.net/api"
-    //"http://localhost:5001/clone-2e61d/us-central1/api"
-});
-
-export default instance; 
